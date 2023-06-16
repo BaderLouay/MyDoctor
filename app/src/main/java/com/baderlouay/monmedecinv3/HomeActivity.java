84620ef3ec83a8b6863ae9b18eea753b275ce314
louay.bader <louay.bader@esprit.tn>
2023-06-16 14:06:35 +0100

    Added doctors details/fixed some errors/trying new things

--- app/src/main/java/com/baderlouay/monmedecinv3/HomeActivity.java
+++ app/src/main/java/com/baderlouay/monmedecinv3/HomeActivity.java
@@ -1,10 +1,13 @@
 package com.baderlouay.monmedecinv3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
@@ -16,6 +19,25 @@ public class HomeActivity extends AppCompatActivity {
 
         SharedPreferences sharedpreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
         String username = sharedpreferences.getString("username","").toString();
-        Toast.makeText(getApplicationContext(), "Welcome "+username, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Welcome back "+username, Toast.LENGTH_SHORT).show();
+
+        CardView exit = findViewById(R.id.cardExit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(HomeActivity.this,LoginActivity.class));
+            }
+        });
+
+        CardView findDoctor = findViewById(R.id.cardFindDoctor);
+        findDoctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,FindDoctorActivity.class));
+            }
+        });
     }
 }
